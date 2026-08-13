c0317d2cf9f24a304a99876deab9db412e90b0da
BasuC-21 <basavarajchougala97@gmail.com>
2026-08-13 11:50:54 +0530

    Add getWatchHistory endpoint to fetch user's watch history with video owner details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -406,6 +406,56 @@ const registerUser = asyncHandler(async( req, res)=>{
 
     })
 
+    const getWatchHistory = asyncHandler(async(req,res)=>{
+        const user = await User.aggregate([
+            {
+                $match:{
+                    _id: new mongoose.Types.ObjectId(req.user._id)
+                }
+            },
+            {
+                $lookup:{
+                    from:"videos",
+                    localField:"watchHistory",
+                    foreignField:"_id",
+                    as:"watchHistory",
+                    pipeline:[
+                        {
+                            $lookup:{
+                                from:"users",
+                                localField:"owner",
+                                foreignField:"_id",
+                                as:"owner",
+                                pipeline:[
+                                    {
+                                        $project:{
+                                            fullName:1,
+                                            username:1,
+                                            avatar:1
+
+
+                                        }
+                                    }
+                                ]
+                            }
+
+                        },
+                        {
+                            $addFields:{
+                                owner:{
+                                    $first:"$owner"
+                                }
+
+                            }
+                        }
+                    ]
+                }
+            }
+        ])
+
+        return res.status(200).json(new ApiResponse(200,user[0].watchHistory,"watch history fetched successfully"))
+    })
+
 
 
 
@@ -418,6 +468,7 @@ getCurrentUser,
 updateAccountDetails,
 updateUserAvatar,
 updateUserCoverImage,
-getUserChannelProfile
+getUserChannelProfile,
+getWatchHistory
 }
 
